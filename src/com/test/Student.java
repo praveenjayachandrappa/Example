992319ab8b332bc6a40f2ab4b62d28108a6da630
Praveen J <27469713+praveenjayachandrappa@users.noreply.github.com>
2019-01-16 15:14:32 +0530

    commit for updated student file

--- src/com/test/Student.java
+++ src/com/test/Student.java
@@ -1,3 +1,4 @@
+package com.test;
 public class Student{
 
 private String sname;
@@ -5,6 +6,31 @@ private String sname;
 private String usn;
 
 private String email;
+
+public String getSname() {
+	return sname;
+}
+
+public void setSname(String sname) {
+	this.sname = sname;
+}
+
+public String getUsn() {
+	return usn;
+}
+
+public void setUsn(String usn) {
+	this.usn = usn;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
 
 
 
