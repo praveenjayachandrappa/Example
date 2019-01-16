ae6fd39cc105ba973ac214bc98e5b071539ba2f2
unknown <Praveen.j@YETHI.IN>
2019-01-16 14:51:11 +0530

    this is first commit into this repo

---
+++ src/com/test/ExampleDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class ExampleDemo {
+
+	public static void method1(){
+		
+		System.out.println("this is method1 in class");
+	}
+	
+	public static void method2(){
+		
+		System.out.println("this is method2 in calss example");
+	}
+}
